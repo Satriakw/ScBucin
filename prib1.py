c2d0807d58965ee635eee2a5f0422d016a16ca8f
Satriakw <145213662+Satriakw@users.noreply.github.com>
2023-09-19 21:04:00 +0700

    Add files via upload

---
+++ prib1.py
@@ -0,0 +1,223 @@
+import os
+import random
+import sys
+import time
+from time import sleep
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.03)
+		
+os.system('clear')
+load = '\x1b[1;91m•'
+count = 0
+
+for x in range(101):
+  time.sleep(0.3)
+  print(f'\r\x1b[1;93mSEDANG MEMPROSES {x}% \x1b[1;91m[{load}]', end='', flush=True)
+  count += 1
+  if count == 3:
+    count = 0
+    load += '\x1b[1;91m•'
+print('      \x1b[1;91m[\x1b[1;96m☠\x1b[1;91m]SELOW BOSKU')
+time.sleep(2)
+
+def slowprint(s):
+    for c in s + '\n':
+      sys.stdout.write(c)
+      sys.stdout.flush()
+      time.sleep(7. / 100)
+      
+slowprint('''\x1b[1;93mProses Selesai ✓''')
+os.system('clear')
+time.sleep(1)
+# Banner
+def banner():
+   os.system('echo "\n\n \033[1;30m<════════════════[\033[1;33;41m • \033[1;37m Bang Satria \033[1;33m• \033[0m\033[1;30m]═══════════════════> \n                                                                  ® author      : Rey Tanoko Driansyah                                                                                            ® facebook    : https://facebook.com/tanoko.iansyah                                                                             ® whatsap     : +6285366010255                                                                                                  ® github      : https://github.com/reytnk                                                                                       ® version     : 1.6.0\n \033[1;30m<════════════════[\033[1;33;41m • \033[1;37m Bang Satria \033[1;33m• \033[0m\033[1;30m]═══════════════════>" |lolcat -a -d 2 -s 50')
+
+def menu():
+  banner()
+  print('                                                              \x1b[1;94m\033[1;30m<═════════════[\033[1;33;41m • \033[1;37m Pilih No Nya  \033[1;33m• \033[0m\033[1;30m]════════════════>')
+  print('                                                            \x1b[1;93m└─> \x1b[1;92m[01] About This Script')
+  print('                                                            \x1b[1;93m└─> \x1b[1;92m[02] Install Package Addition')
+  print('                                                            \x1b[1;93m└─> \x1b[1;92m[03] Tools Bucin Ges')
+  print('                                                            \x1b[1;93m└─> \x1b[1;92m[04] Exit Script ')     
+  print('                                                              \x1b[1;94m\033[1;30m<══════════════[\033[1;33;41m • \033[1;37mMASUKIN Nomor \033[1;33m• \033[0m\033[1;30m]═════════════════>')
+  pilih = input('\x1b[1;93m└─> \x1b[1;92mPilih mana bro? : ')
+  if pilih in ['1','01']:
+    imp()
+  elif pilih in ['2','02']:
+    add()
+  elif pilih in ['3','03']:
+    semua()
+  elif pilih in ['4','04']:
+    print('\x1b[1;92m✓')
+    jalan(f'\x1b[1;93mThanks sudah menggunakan tools ini')
+    print('\x1b[1;92m✓')
+    jalan(f'\x1b[1;93mJika ada yang kurang tolong hubungi author')
+    input('                                                                \x1b[1;93m└─> \x1b[1;92mTekan Enter Sekali Lagi Untuk Log Out')
+    os.system('clear')
+    exit()
+    try:
+      menu()
+    except:pass
+    exit()
+  else:jalan('\x1b[1;91m●═════════════════════════════════════════════════════════════● Isi yang benar dong bos-_')
+  print('\x1b[1;93m●═════════════════════════════════════════════════════════════● Log out or Re-login?')
+  kemana = input('\x1b[1;92mOut or Re? : ')
+  if kemana in ['Out','out']:
+    slowprint('''\x1b[1;96mG O O D B Y E B R O     ☠⃝     ☠⃝     ☠⃝     ☠⃝     ☠⃝     ☠⃝     ☠⃝⃝⃝⃝''')
+    time.sleep(3)
+    os.system('clear')
+    exit()
+  elif kemana in ['Re','re']:
+    slowprint('''\x1b[1;96mW E L C O M E B A C K T O      ☠⃝     ☠⃝     ☠⃝     ☠⃝     ☠⃝     ☠⃝''')
+    time.sleep(3)
+    os.system('clear')
+    menu()
+  
+
+def imp():
+  slowprint('''Pilih Bahasa Anda | Choose Your Language''')
+  print ('\x1b[1;94mType I/i For Indonesian')
+  print ('\x1b[1;94mType E/e For English')
+  lanjut = input('Type? : \x1b[1;97m')
+  if lanjut in ['i','I']:
+    print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+    slowprint('''\x1b[1;97m[•] Tools ini adalah tools kegabutan yang di buat oleh''')
+    print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+    slowprint('''\x1b[1;97m[•] ☠.Rey Tanoko Driansyah.☠''')
+    print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+    slowprint('''\x1b[1;97m[•] Jika ingin memahami lebih banyak tentang percodingan.''')
+    print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+    slowprint('''\x1b[1;97m[•] Silahkan hubungi author, kontak tersedia di banner.''')
+    print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+    print('\x1b[1;94mTekan Enter untuk kembali ke menu')
+    input('\x1b[1;94mEnter')
+    os.system('clear')
+    menu()
+  elif lanjut in ['e','E']:
+    print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+    slowprint('''\x1b[1;97m[•] This tool is a combination tool made by Rey Tanoko Driansyah''')
+    print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+    slowprint('''\x1b[1;97m[•] If you want to understand more about coding''')
+    print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+    slowprint('''\x1b[1;97m[•] Please contact the author, contact is available on the          banner''')
+    print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+    print('\x1b[1;94mPress Enter to return to the menu')
+    input('Enter')
+    os.system('clear')
+    menu()
+    
+def add():
+  slowprint('''\n\x1b[1;91mApakah anda yakin akan Menginstall package ini?...''')
+  print ('\n\x1b[1;97mKetik Y jika ingin Menginstall')
+  print ('\n\x1b[1;91mKetik N jika tidak ingin Menginstall')
+  lanjut = input('\nYes or No? : \x1b[1;97m')
+  if lanjut in ['y','Y']:
+     jalan('\n\x1b[1;97mTunggu sebentar kawan...')
+     slowprint('''\n\x1b[1;91mPenginstallan dimulai......''')
+     jalan('\n\x1b[1;97mMohon bersabar anda sedang Menginstall module')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module python \x1b[1;91m')
+     os.system('pkg install python')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Php \x1b[1;91m')
+     os.system('pkg install php')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Nano \x1b[1;91m')
+     os.system('pkg install nano')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Git \x1b[1;91m')
+     os.system('pkg install git')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Wget \x1b[1;91m')
+     os.system('pkg install wget')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Toilet \x1b[1;91m')
+     os.system('pkg install toilet')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Curl \x1b[1;91m')
+     os.system('pkg install curl')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Jq \x1b[1;91m')
+     os.system('pkg install jq')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Bash \x1b[1;91m')
+     os.system('pkg install bash')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Nodejs \x1b[1;91m')
+     os.system('pkg install nodejs')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Pip Requests \x1b[1;91m')
+     os.system('pip install requests')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Pip Keyboard \x1b[1;91m')
+     os.system('pip install keyboard')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Pip2 requests \x1b[1;91m')
+     os.system('pip2 install requests')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     print ('☠\x1b[1;93mMenginstall Module Pip2 Mechanize \x1b[1;91m')
+     os.system('pip2 install mechanize')
+     print('                                                                \x1b[1;94m●═════════════════════════════════════════════════════════════●')
+     slowprint('''\x1b[1;91mS E L E S A I ........... You automatically return to the menu''')
+     os.system('clear')
+     menu()
+  elif lanjut in ['n','N']:
+    jalan('\x1b[1;91mSampai jumpa kawan:/')
+    os.system('clear')
+    menu()
+    
+def semua():
+  os.system('clear')
+  banner()
+  print('\n\033[1;93mMasukan nama pasangan kamu')
+  name=input('\n\x1b[1;97mNama? : ')
+  print('                                                                \x1b[1;93m[♡] \x1b[1;97m Satria ♡ '+name)
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  slowprint('''\x1b[1;93m[♡] \x1b[1;96mKamu tau enggak sih... \x1b[1;97m'''+name)
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  time.sleep(1)
+  slowprint('''\x1b[1;93m[♡] \x1b[1;96mKamu itu manisssss banget😚''')
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  time.sleep(1)
+  slowprint('''\x1b[1;93m[♡] \x1b[1;96mAku itu beruntung banget milikin kamu:/''')
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  time.sleep(1)
+  slowprint('''\x1b[1;93m[♡] \x1b[1;96mKamu udah jadi rumah bahkan segalanya ♡''')
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  time.sleep(1)
+  slowprint('''\x1b[1;93m[♡] \x1b[1;96mJangan berubah yah manis><''')
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  time.sleep(1)
+  slowprint('''\x1b[1;93m[♡] \x1b[1;96mTetap seperti sekarang''')
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  time.sleep(1)
+  slowprint('''\x1b[1;93m[♡] \x1b[1;96mJadi orang yang paling aku banggakan (っ˘з(˘⌣˘ )''')
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  time.sleep(1)
+  slowprint('''\x1b[1;93m[♡] \x1b[1;96mJangan khawatir yah''')
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  time.sleep(1)
+  slowprint('''\x1b[1;93m[♡] \x1b[1;96mAku disini akan terus mempertahankan hubungan kita''')
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  time.sleep(1)
+  slowprint('''\x1b[1;93m[♡] \x1b[1;96mTerimakasih atas kasih sayang dan ketulusannya ♡''')
+  print('\x1b[1;97m●════════════════════════════════════════════════════════●')
+  time.sleep(1)
+  slowprint('''                       \x1b[1;91m ▄████▄████▄
+                       ██▒▒▒▒█▒▒▒▒██
+                      ▀██ ▒▒▒ ▒▒▒ ██▀
+                        ▀██▒▒▒▒▒██▀
+                          ▀██▒██▀
+                            ▀█▀''')
+  print('\033[1;30m<══════════════════[\033[1;33;41m • \33[37;1m Tekan Enter  \033[1;33m• \033[0m\033[1;30m]═════════════════════>')
+  print('\033[1;93mTekan Enter Untuk Kembali...')
+  input('\033[1;90mEnter')
+  os.system('clear')
+  menu()
+
+menu()
